841932d602b6ac147a38a02d3da0d51b5a6ae331
Vidotto <vitotbrbr@gmail.com>
2024-11-25 09:50:50 -0300

    cleaning code

--- src/app/renomeadores/page.tsx
+++ src/app/renomeadores/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 import Navbar from "../components/NavBar";
-import RenameFilesApp from "../components/RenameFilesApp";
-import RenameStagesApp from "../components/RenameStages";
 import RenameLinkGrid from "../components/Renomeadores";
 
 export default function Home() {
