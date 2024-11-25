f4cd37a3759f87172bcf08f6bb05bcb689e9cf12
Vidotto <vitotbrbr@gmail.com>
2024-11-25 11:22:28 -0300

    configuration backend

--- src-tauri/src/scripts/mod.rs
+++ src-tauri/src/scripts/mod.rs
@@ -1,3 +1,5 @@
 pub mod rename_files; // Exporte o módulo rename_files
 pub mod renomear_fases; // Exporte o módulo renomear_fases
 pub mod list_files; // Exporte o módulo list_files
+pub mod set_window_size;
+pub mod set_fullscreen;
