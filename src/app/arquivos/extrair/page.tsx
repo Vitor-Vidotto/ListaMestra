224dddcdbc682268ea531b2c3fa5c9fe88c20b40
Vitor-vidotto <vitotbrbr@gmail.com>
2025-05-12 12:00:04 -0300

    import fixes

--- src/app/arquivos/extrair/page.tsx
+++ src/app/arquivos/extrair/page.tsx
@@ -7,7 +7,7 @@ import ExtrairParedes from "@/app/components/ExtrairParedes";
 import ExtrairAberturas from "@/app/components/ExtrairAberturas";
 import FileUploader from "@/app/components/FileUploader";
 import { useState } from "react";
-import ExtrairEixos from "@/app/components/ExtrairEixos";
+import ExtrairFerros from "@/app/components/ExtrairFerros";
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null);
@@ -21,7 +21,7 @@ export default function Home() {
         <ExtrairBlocos file={file} />
         <ExtrairParedes file={file} />
         <ExtrairAberturas file={file} />
-        <ExtrairEixos file={file} />
+        <ExtrairFerros file={file} />
       </div>
       </main>
     </div>
